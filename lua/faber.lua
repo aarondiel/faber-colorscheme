95765697d51420b263efb40c257c725154f8c6fa
aarondiel <aaron.diel@t-online.de>
2022-09-22 15:14:10 +0200

    feat(using the nvim lua api)

--- lua/faber.lua
+++ lua/faber.lua
@@ -1,54 +1,8 @@
-local faber = {}
 local none_color = { color = "NONE", fallback = "NONE" }
+local faber = {}
 
-faber.styles = {
-	bold = "bold",
-	underline = "underline",
-	underline_double = "underlineline",
-	underline_curly = "undercurl",
-	underline_dotted = "underdot",
-	underline_dashed = "underdash",
-	strikethrough = "strikethrough",
-	inverse_foreground_background = "reverse",
-	italic = "italic",
-	standout = "standout",
-	none = "nocombine"
-}
-
-faber.colors = { none = none_color }
-
-local function filter(list, condition_function)
-	local result = {}
-
-	for _, item in ipairs(list) do
-		if condition_function(item) then
-			table.insert(result, item)
-		end
-	end
-
-	return result
-end
-
-local function object_map(obj, func)
-	local result = {}
-
-	for name, element in pairs(obj) do
-		result[name] = func(name, element)
-	end
-
-	return result
-end
-
-local function object_filter(obj, func)
-	local result = {}
-
-	for name, element in pairs(obj) do
-		if func(name, element) then
-			result[name] = element
-		end
-	end
-
-	return result
+local function opt_return(opt)
+	return opt
 end
 
 local function get_true_color(color)
@@ -68,52 +22,71 @@ local function get_fallback_color(color)
 
 	local fallback_color = color.fallback or color[2]
 
-	return tostring(fallback_color)
+	return fallback_color
 end
 
-local function calculate_color_arguments(color, fg_bg)
-	if color == nil then
-		return ""
-	end
-
-	local true_color = get_true_color(color)
-	local fallback_color = get_fallback_color(color)
-
-	local gui = ""
-	local cterm = ""
-
-	if true_color ~= nil then
-		gui = "gui" .. fg_bg .. "=" .. true_color
-	end
-
-	if fallback_color ~= nil then
-		cterm = "cterm" .. fg_bg .. "=" .. fallback_color
-	end
-
-	return vim.fn.trim(table.concat({ gui, cterm }, " "))
-end
-
-local function calculate_style_argument(style)
-	local result_style = ""
-
-	if style == nil then
+local function get_opt_return(style)
+	if (
+		style ~= nil and
+		type(style) == "function"
+	) then
+		return style()
+	else
 		return nil
 	end
+end
 
-	if type(style) == "string" then
-		result_style = style
-	elseif type(style) == "table" then
-		local filtered_style = filter(style, function(style_argument)
-			return style_argument ~= faber.styles.none
-		end)
+local function get_special_color(style)
+	return (
+		get_opt_return(style.underline) or
+		get_opt_return(style.underline_double) or
+		get_opt_return(style.underline_curly) or
+		get_opt_return(style.underline_dotted) or
+		get_opt_return(style.underline_dashed)
+	)
+end
 
-		result_style = table.concat(filtered_style, ",")
+faber.styles = {
+	bold = true,
+	underline = opt_return,
+	underline_double = opt_return,
+	underline_curly = opt_return,
+	underline_dotted = opt_return,
+	underline_dashed = opt_return,
+	strikethrough = true,
+	reverse_foreground_background = true,
+	italic = true,
+	standout = true
+}
+
+faber.colors = { none = none_color }
+
+--- type group = {
+-- 	fg: Color,
+-- 	bg: Color,
+-- 	style: Style, faber.styles.underline(Color),
+-- 	default: boolean,
+-- 	link: String
+-- }
+
+function faber.get_true_colors()
+	local result = {}
+
+	for color_name, color_value in pairs(faber.colors) do
+		result[color_name] = get_true_color(color_value)
 	end
 
-	assert(type(result_style) == "string", "invalid type for style")
-	assert(result_style ~= "", "invalid style")
+	return result
+end
 
-	return "gui=" .. result_style .. " " .. "cterm=" .. result_style
+function faber.get_fallback_colors()
+	local result = {}
+
+	for color_name, color_value in pairs(faber.colors) do
+		result[color_name] = get_fallback_color(color_value)
+	end
+
+	return result
 end
 
 function faber.register_colors(colors)
@@ -123,70 +96,45 @@ function faber.register_colors(colors)
 	return faber.colors
 end
 
-function faber.get_true_colors()
-	local result = object_map(faber.colors, function(_, value)
-		return get_true_color(value)
-	end)
-
-	result = object_filter(result, function(name, _)
-		return name ~= "none"
-	end)
-
-	return result
-end
-
-function faber.get_fallback_colors()
-	local result = object_map(faber.colors, function(_, value)
-		return get_fallback_color(value)
-	end)
-
-	result = object_filter(result, function(name, _)
-		return name ~= "none"
-	end)
-
-	return result
-end
-
-function faber.clear_highlight_group(group_name)
-	vim.cmd("highlight clear " .. group_name)
-end
-
-function faber.link_highlight_group(from_group, to_group)
-	assert(
-		type(from_group) == "string",
-		"invalid from_group for link_highlight_group"
-	)
-
-	assert(
-		type(to_group) == "string",
-		"invalid to_group for link_highlight_group"
-	)
-
-	vim.cmd("highlight! link " .. from_group .. " " .. to_group)
-end
-
-function faber.highlight_group(group_name, foreground, background, style)
-	local command = {
-		"highlight",
-		group_name,
-		calculate_color_arguments(foreground, "fg"),
-		calculate_color_arguments(background, "bg"),
-		calculate_style_argument(style)
-	}
-
-	command = filter(command, function(cmd)
-		return cmd ~= ""
-	end)
-
-	if #command == 2 then
-		return
+function faber.highlight_group(name, fg, bg, style, default)
+	if style == nil then
+		style = {}
+	elseif type(style) ~= "table" then
+		style = { style }
 	end
 
-	vim.cmd(table.concat(command, " "))
+	vim.api.nvim_set_hl(0, name, {
+		fg = get_true_color(fg),
+		bg = get_true_color(bg),
+		sp = get_special_color(style),
+		font = get_opt_return(style.font),
+		bold = style.bold or false,
+		italic = style.italic or false,
+		reverse = style.reverse_foregroun_background or false,
+		standout = style.standout or false,
+		underline = style.underline or false,
+		underlineline = style.underline_double or false,
+		undercurl = style.unerline_curly or false,
+		underdot = style.underline_dotted or false,
+		underdash = style.underline_dashed or false,
+		strikethrough = style.strikethrough or false,
+		default = default or false,
+		ctermfg = get_fallback_color(fg),
+		ctermbg = get_fallback_color(bg)
+	})
+end
+
+function faber.link_highlight_group(from, to)
+	vim.cmd("highlight! link " .. from .. " " .. to)
 end
 
 function faber.highlight_groups(groups)
 	for group_name, group_settings in pairs(groups) do
+		if group_settings.link ~= nil then
+			faber.link_highlight_group(group_name, group_settings.link)
+			goto continue
+		end
+
 		local foreground = group_settings["foreground"] or
 			group_settings["fg"] or
 			group_settings[1]
@@ -195,22 +143,21 @@ function faber.highlight_groups(groups)
 			group_settings["bg"] or
 			group_settings[2]
 
-		local style = group_settings["style"] or group_settings[3]
+		local style = group_settings["style"] or
+			group_settings[3]
 
-		if group_settings["clear"] then
-			faber.clear_highlight_group(group_name)
-		end
-
-		if group_settings["link"] ~= nil then
-			faber.link_highlight_group(group_name, group_settings["link"])
-		end
+		local default = group_settings["default"] or
+			group_settings[4]
 
 		faber.highlight_group(
 			group_name,
 			foreground,
 			background,
-			style
+			style,
+			default
 		)
+
+		::continue::
 	end
 end
 
